6eb323eb48535fa6b29b9ad382d4f0bc43b70041
Amet-Vikram <amet1197@gmail.com>
2022-02-08 11:37:13 +0530

    Added part time functionality with Case statement

--- empWage.sh
+++ empWage.sh
@@ -1,15 +1,27 @@
 #!/bin/bash 
 
-isPresent=1
-randcheck=$((RANDOM%2))
+isFullTime=1
+isPartTime=2
+randcheck=$((RANDOM%3))
 wage=30
-isFulltime=$((RANDOM%9))
-if [ $isPresent -eq $randcheck ]
-then
-	echo "Present"
-	salary=$(($wage*$isFulltime))
-	echo "He gets: " $salary
-else
-	echo "Absent!! He gets nothing. Lol!"	
-fi
+# Case Statement
+case $randcheck in 
+	$isFullTime) emphrs=8
+					;;
+	$isPartTime) emphrs=4
+					;;
+				 *) emphrs=0
+					;;
+esac 
+
+salary=$(($wage*$emphrs))
+echo $salary
+#if [ $isPresent -eq $randcheck ]
+#then
+#	echo Present
+#	salary=$(($wage*$isFulltime))
+#	echo He gets:  $salary
+#else
+#	echo Absent!! He gets nothing. Lol!	
+#fi
 

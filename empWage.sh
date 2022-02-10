b6c155ec1849f13a640edce2a291ff42e18d76f0
Amet-Vikram <amet1197@gmail.com>
2022-02-10 12:25:15 +0530

    Added functionality of checking wages for each day

--- empWage.sh
+++ empWage.sh
@@ -11,27 +11,39 @@ maxWorkinghrs=100
 totalWorkingdays=0
 #for (( days=1; days<=$maxworkingday; days++ ))
 # Case Statement
+
+function getWorkinghr() {
+
+	case $1 in
+   	   $isFullTime) emphrs=8
+   	               ;;
+	      $isPartTime) emphrs=4
+         	         ;;
+      	          *) emphrs=0
+            	      ;;
+	esac
+	echo $emphrs
+}
+
+function getEmpWagePerDay(){
+	echo $(($1 * $wage))
+}
+
 while [[ $totalWorkinghrs -lt $maxWorkinghrs && $totalWorkingdays -le $maxworkingday ]]
 do
 	((totalWorkingdays++))
 	randcheck=$((RANDOM%3))
 
-	case $randcheck in 
-		$isFullTime) emphrs=8
-						;;
-		$isPartTime) emphrs=4
-						;;
-					 *) emphrs=0
-						;;
-	esac 
+	emphrs=$( getWorkinghr $randcheck )
 	totalWorkinghrs=$(($totalWorkinghrs+$emphrs))
 	salary=$(($wage*$emphrs))
 	totalSalary=$(($totalSalary+$salary))
+	dailyWage[$totalWorkingdays]=$( getEmpWagePerDay $emphrs )
 done
 
 echo "Hours Worked: " $totalWorkinghrs
 echo "Monthly Salary is: " $totalSalary
-
+echo "Daily Wage: " ${dailyWage[@]}
 
 #if [ $isPresent -eq $randcheck ]
 #then

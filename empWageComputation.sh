6158af22a95a6eda54c4e55eb6250ac21387b616
Amet-Vikram <amet1197@gmail.com>
2022-02-19 12:28:36 +0530

    Calculate Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,9 +6,13 @@ echo 'Welcome to Employee Wage Computation Program'
 
 isPresent=1
 randcheck=$((RANDOM%2))
+wage=30
+isFulltime=$((RANDOM%9))
+
 if [ $isPresent -eq $randcheck ]
 then
-	echo "Present"
+	salary=$(($wage*$isFulltime))
+	echo "He is Present. He gets: " $salary
 else
-	echo "Absent!"
+	echo "Absent!! He gets nothing. Lol!"	
 fi
